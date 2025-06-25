f00c37bad391fc0e901bcb57530b01072c337358
Kathy Forero <143484307+kathyforero@users.noreply.github.com>
2025-06-25 14:33:32 +0000

    Guía 13 Completa

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import { Grid } from '@mui/material';
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import HeaderUI from './components/HeaderUI';
+import './App.css';
+import AlertUI from './components/AlertUI';
 
 function App() {
   return (
     <Grid container spacing={5} justifyContent="center" alignItems="center">
 
         {/* Encabezado */}
-        <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
+        <Grid size={{ xs: 12, md: 12 }}><HeaderUI/></Grid>
 
         {/* Alertas */}
-        <Grid size={{ xs: 12, md: 12 }}>Elemento: Alertas</Grid>
+        <Grid size={{ xs: 12, md: 12 }} container justifyContent="right" alignItems="center">
+
+             <AlertUI description="No se preveen lluvias"/></Grid>
 
         {/* Selector */}
         <Grid size={{ xs: 12, md: 3  }}>Elemento: Selector</Grid>
